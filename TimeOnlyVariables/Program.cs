525ac072dc6627ec98983a02eb84a322245a80be
antjamespadgett <padgettanthonyj@gmail.com>
2022-01-28 10:09:18 -0600

    finished project

---
+++ TimeOnlyVariables/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+//TimeOnly opensAt = TimeOnly.Parse("8:00 AM");
+
+//Console.WriteLine(opensAt);
+
+TimeOnly closeAt = TimeOnly.Parse("9:00 PM");
+Console.WriteLine(closeAt);
+
+
+TimeOnly rightNow = TimeOnly.FromDateTime(DateTime.Now);
+Console.WriteLine(rightNow);
+
+
+TimeOnly opensAt = TimeOnly.Parse("8:30 PM");
+Console.WriteLine(opensAt);
+
+Console.ReadLine();
